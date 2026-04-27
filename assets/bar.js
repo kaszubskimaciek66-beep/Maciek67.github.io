b7d6816df3735f8746e55f0efd3ac49959a1f58e
kaszubskimaciek66-beep <kaszubskimaciek66@gmail.com>
2026-04-27 23:22:07 +0200

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -1,78 +1,64 @@
-
-var params = new URLSearchParams(window.location.search);
-
-var bar = document.querySelectorAll(".bottom_element_grid");
-
-var top = localStorage.getItem('top');
-var bottom;
-
-if (localStorage.getItem('bottom')){
-    bottom = localStorage.getItem('bottom');
-
-    bar.forEach((element) => {
-        var image = element.querySelector('.bottom_element_image');
-        var text = element.querySelector('.bottom_element_text');
-
-        var send = element.getAttribute('send');
-        if (send === bottom){
-            image.classList.add(bottom + "_open");
-            text.classList.add("open");
-        }else{
-            image.classList.remove(send + "_open");
-            image.classList.add(send);
-            text.classList.remove("open");
-        }
-    })
-}
-
-function sendTo(url, top, bottom){
-    if (top){
-        localStorage.setItem('top', top)
+document.addEventListener("DOMContentLoaded", () => {
+    // Pobranie zdjęcia z pamięci i wstawienie do kafelka mDowód
+    const photo = localStorage.getItem('image');
+    const humanImg = document.querySelector(".human");
+    if (photo && humanImg) {
+        humanImg.src = photo;
     }
-    if (bottom){
-        localStorage.setItem('bottom', bottom)
+    
+    // Opcjonalnie zmiana napisu "mDowód" na Imię i Nazwisko
+    const name = localStorage.getItem('name');
+    const surname = localStorage.getItem('surname');
+    const cardTitle = document.querySelector(".name");
+    if (name && surname && cardTitle) {
+        // cardTitle.innerHTML = name + " " + surname; // Odkomentuj jeśli chcesz nazwisko na kafelku
     }
-    location.href = `/${url}?` + params;
-}
+});
 
-var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-var optionsTime = { second: '2-digit', minute: '2-digit', hour: '2-digit' };
+// Ta funkcja obsługuje kliknięcia w kafelki (onclick="sendTo(...)")
+function sendTo(page) {
+    // Mapujemy nazwy z HTML na Twoje pliki .html
+    const pages = {
+        'home': 'home.html',
+        'documents': 'id.html', // W mObywatelu dokumenty to często główny ID
+        'services': 'services.html',
+        'qr': 'qr.html',
+        'more': 'more.html',
+        'card': 'id.html' // Kliknięcie w mDowód przenosi do id.html
+    };
 
-bar.forEach((element) => {
-    element.addEventListener('click', () => {
-        localStorage.removeItem('top');
-        localStorage.removeItem('bottom');
+    const target = pages[page];
 
-        sendTo(element.getAttribute("send"))
-    })
-})
-
-function getMobileOperatingSystem() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  
-    if (/windows phone/i.test(userAgent)) {
-        return 1;
+    if (target) {
+        window.location.href = target;
+    } else {
+        console.error("Nie znaleziono strony dla: " + page);
     }
-  
-    if (/android/i.test(userAgent)) {
-        return 2;
-    }
-  
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return 3;
-    }
-  
-    return 4;
-}
-  
-if (getMobileOperatingSystem() == 2){
-    document.querySelector(".bottom_bar").style.height = "70px"
 }
 
-function getRandom(min, max) {
-    return parseInt(Math.random() * (max - min) + min);
-}
+// Obsługa dolnego paska (ikonki na dole)
+document.addEventListener("DOMContentLoaded", () => {
+    const bottomElements = document.querySelectorAll(".bottom_element_grid");
 
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
+    bottomElements.forEach(element => {
+        element.addEventListener("click", () => {
+            const destination = element.getAttribute("send");
+            if (destination) {
+                sendTo(destination);
+            }
+        });
+    });
+    
+    // Dodatkowo: Obsługa kafelków usług (Bezpiecznie w sieci, PESEL itd.)
+    const serviceBoxes = document.querySelectorAll(".service_box");
+    serviceBoxes.forEach(box => {
+        box.addEventListener("click", () => {
+            const name = box.querySelector(".service_box_name").innerText;
+            if (name.includes("PESEL")) {
+                window.location.href = "pesel.html";
+            } else if (name.includes("pojazdu")) {
+                window.location.href = "vehicle.html"; // Jeśli go masz
+            }
+        });
+    });
+});
